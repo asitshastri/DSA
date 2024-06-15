f252ab46388c1118cc6b390ef344480a4a51f015
asitshasti <asitshastri@gmail.com>
2024-06-16 03:06:55 +0530

    assignment

--- Array/test.java
+++ Array/test.java
@@ -1,20 +1,31 @@
 package Array;
 /**
- * Q4: Write a program to find out the second largest element in a given array.
-Input 1: arr[] = {34,21,54,65,43}
+ * Q5: Write a function which accepts a 2D array of integers and its size as arguments
+ *  and displays the elements of middle row and the elements of middle column. 
+ * Printing can be done in any order.
+[Assuming the 2D Array to be a square matrix with odd dimensions i.e. 3x3, 5x5, 7x7 
+etc...]
  */
 public class test {
-
     public static void main(String[] args){
-        int peak = -1;
-        int arr[] = {1, 4,7,3,2,6,5};
-        for(int i=1;i<arr.length-1;i++){
-            if(arr[i]>arr[i-1] && arr[i]>arr[i+1]){
-                if(peak<arr[i]){
-                    peak = arr[i];
+        
+        int arr[][]={{1,2,3,4,5},{6,7,8,9,10},{11,12,13,14,15},{16,17,18,19,20},{21,22,23,24,25}};
+        System.out.print("Middle Row: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(i==arr.length/2){
+                    System.out.print(arr[i][j]+" ");
                 }
-            }  
+            }
         }
-        System.out.println(peak);
+        System.out.print("\nMiddle Column: ");
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                if(j==arr.length/2){
+                    System.out.print(arr[i][j]+" ");
+                }
+            }
+        }
+        
     }
 }
